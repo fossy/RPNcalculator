cf741a7d02fc48d76cfb26413708eb534b41bd45
J.Fossy Weinzinger <fy.github@iwasno.net>
2024-11-25 18:48:30 +0100

    web page help

---
+++ web-page-help/user-data-policy.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>User Data Policy</title>
+</head>
+<body>
+<h1>RPNcalculator</h1>
+<h2>Datenschutzerklärung</h2>
+<p>Die App RPNcalculator ermittelt keine Benutzerdaten und speichert auch keine.
+</p>
+<h2>User Data Policy</h2>
+<p>The app RPNcalculator does not determine any user data and does not store any.
+</p>
+</body>
+</html>
